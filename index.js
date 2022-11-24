6417078d9b8754b22e588c0a25976ea9722c616e
Linet Oliech <linetaoliech@gmail.com>
2022-11-24 22:03:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
-// Code your solution here
+function reducer(totalBatteries, batteryBatch) {
+  return totalBatteries + batteryBatch;
+}
+const totalBatteries = batteryBatches.reduce(reducer, 0);
